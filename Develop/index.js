ae94dfb9bfac4b5bd9b0db0f2c12e4d8d8e60833
github <calbest31@outlook.com>
2021-04-28 00:21:08 -0400

    array of questions done

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,37 @@ const fs = require('fs')
 
 // TODO: Create an array of questions for user input
 const questions = [
-
+    {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email', 
+    },
+    {
+        type: 'input',
+        message: 'What is your projects name?',
+        name: 'projectid',
+    },
+    {
+        type: 'input',
+        message: 'Please provide a short description of your project...',
+        name: 'decription',
+    },
+    {
+        type: 'list',
+        message: 'What kind of license does your project need?',
+        options: ['MIT','APACHE 2.0','GPL 3.0', 'BSD 3', 'Other'],
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'Please name your collaborators...',
+        name: 'collaborators',
+    }
 ];
 
 // TODO: Create a function to write README file
